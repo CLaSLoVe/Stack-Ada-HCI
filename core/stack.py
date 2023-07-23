c50d59e30ac912e24bc17b058896e3b9d6ceab5e
CLaSLoVe <jindian_0805@foxmail.com>
2023-07-23 15:46:48 +0800

    stack construct

--- core/stack.py
+++ core/stack.py
@@ -1,2 +1,24 @@
-def stack():
-    return
+def aoi2panels(data):
+    panels = data
+    return panels
+
+
+class Stack:
+    def __init__(self, par_namelist):
+        self.par_namelist = par_namelist
+        self.stack = dict(zip(par_namelist, [0] * len(par_namelist)))
+        self.actual = dict(zip(par_namelist, [0] * len(par_namelist)))
+        self.bias = dict(zip(par_namelist, [0] * len(par_namelist)))
+
+    def get(self, data):
+        tag, data = data
+        if tag == 'aoi':
+            panels = aoi2panels(data)
+            for panel in panels:
+                self.stack[panel] = self.actual[panel]
+        elif tag == 'par':
+            self.actual.update(data)
+
+    def get_bias(self):
+        diff_dict = {key: self.stack[key] - self.actual[key] for key in self.par_namelist}
+        return diff_dict
